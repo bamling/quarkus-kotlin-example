5d18b417f190a6c7b20780016505202f039d7d73
bamlin <b.amling@tarent.de>
2019-09-06 16:14:02 +0200

    working json deserialisation using jackson

--- src/main/kotlin/com/github/bamling/Fruit.kt
+++ src/main/kotlin/com/github/bamling/Fruit.kt
@@ -1,10 +1,13 @@
 package com.github.bamling
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.bson.types.ObjectId
 
-data class Fruit(
-        @JsonIgnore val id: ObjectId? = null,
-        val name: String = "",
-        val description: String = ""
-)
+data class Fruit(@JsonIgnore val id: ObjectId? = null, val name: String = "", val description: String = "") {
+
+    @JsonCreator
+    constructor(@JsonProperty("name") name: String = "",
+                @JsonProperty("description") description: String = "") : this(null, name, description)
+}
