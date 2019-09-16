fd1af1d4acd4c6c6d35cf0029bc1a47c9e51b551
bamlin <b.amling@tarent.de>
2019-09-16 15:06:54 +0200

    fixed json serialization of Kotlin data class

--- src/main/kotlin/com/github/bamling/Fruit.kt
+++ src/main/kotlin/com/github/bamling/Fruit.kt
@@ -1,13 +1,6 @@
 package com.github.bamling
 
-import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnore
-import com.fasterxml.jackson.annotation.JsonProperty
 import org.bson.types.ObjectId
 
-data class Fruit(@JsonIgnore val id: ObjectId? = null, val name: String = "", val description: String = "") {
-
-    @JsonCreator
-    constructor(@JsonProperty("name") name: String = "",
-                @JsonProperty("description") description: String = "") : this(null, name, description)
-}
+data class Fruit(@JsonIgnore var id: ObjectId? = null, var name: String = "", var description: String = "")
